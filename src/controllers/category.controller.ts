82c425d5ec9b8459b64a2aa73e9ee399a9b2e4c7
Davi Guarda <davi.guarda.b@hotmail.com>
2024-02-03 17:22:10 -0300

    feat: add new endpoint for category

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -18,6 +18,24 @@ export const getAllCategories = async (request: AuthRequest, response: Response)
     }
 }
 
+export const getCategoryById = async (
+    request: AuthRequest,
+    response: Response
+  ) => {
+    try {
+      const { user } = request
+      const { id } = request.params
+      const category = await Category.findOne({
+        _id: id,
+      })
+      return response.send(category)
+    } catch (error) {
+      response.send({ error: "Something went wrong" })
+      console.log("error in getAllCategories", error)
+      throw error
+    }
+  }
+
 export const createCategory = async (request: AuthRequest, response: Response) => {
     try {
         const { color, icon, isEditable, name }: ICategory = request.body
